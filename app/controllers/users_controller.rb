aa135699b39138597f3494f2981cddfab638976f
jfonz412 <jfonzone@outlook.com>
2017-05-29 15:59:22 -0400

    edit/update

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,14 +4,21 @@ class UsersController < ApplicationController
 	end
 
 	def create
+		#@user = User.new(username: params[:username], email: params[:email], password: params[:password])	
 		whitelisted_params = user_params
 		@user = User.new(whitelisted_params)
-		#@user = User.new(username: params[:username], email: params[:email], password: params[:password])
+		
 		if @user.save
 			redirect_to new_user_path
 		else
 			render :new
 		end
+	end
+
+	def edit
+	end
+
+	def update
 	end
 
 	private
