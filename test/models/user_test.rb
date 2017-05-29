8032c73d10e8d785878fe121a5fdaa60d341fddf
jfonz412 <jfonzone@outlook.com>
2017-05-29 13:23:17 -0400

    User presence validations

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,26 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+  	@user = User.new(username: "Mr. Foo", email: "Foo@email.com", password: "pass123")
+  end
+
+  test "should be valid" do
+  	assert(@user.valid?)
+  end
+
+  test "username can't be blank" do
+  	@user.username = "			"
+  	assert_not(@user.valid?)
+  end
+
+  test "email can't be blank" do
+  	@user.email = "			"
+  	assert_not(@user.valid?)
+  end
+
+  test "password can't be blank" do
+  	@user.password = "			"
+  	assert_not(@user.valid?)
+  end
 end
