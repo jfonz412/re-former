4f7d092044003449be3d194b6d74af2165070e68
jfonz412 <jfonzone@outlook.com>
2017-05-29 16:16:25 -0400

    finished

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -16,9 +16,20 @@ class UsersController < ApplicationController
 	end
 
 	def edit
+		@user = User.find(params[:id])
 	end
 
 	def update
+		@user = User.find(params[:id])
+
+		whitelisted_params = user_params
+		@user.update_attributes(user_params)
+		
+		if @user.save
+			redirect_to new_user_path
+		else
+			render :edit
+		end
 	end
 
 	private
